d9132573cf1bc4be624158c37be179784e5122dc
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2025-11-04 20:54:32 +0000

    feat:Implement usage of scanf to read data

--- Data Types & Variables/UserInput.go
+++ Data Types & Variables/UserInput.go
@@ -3,8 +3,23 @@ import "fmt"
 
 //Knowing how to read data inputted in the terminal
 func main()  {
-	var name string
-	fmt.Print("Please input your name: ")
-	fmt.Scanf("%s", &name)
-	fmt.Println("Welcome, " name)
+	//var name string
+	//fmt.Print("Please input your name: ")
+	//fmt.Scanf("%s", &name)
+	//fmt.Println("Welcome,", name)
+
+	//The fmt.ScanF returns two values, which are:
+	//1. Count(n): The number of successful scanned Items
+	//2. err: An error that indicates what went wrong
+	//For example:
+	var a string
+	var b int
+	fmt.Print("Enter a string and an integer: ")
+	count, err := fmt.Scanf("%s", "%d", &a, &b)
+
+	//Demonstrating how scanf works
+	fmt.Println("Count:", count)
+	fmt.Println("error:", err)
+	fmt.Println("a:", a)
+	fmt.Println("b:", b)
 }
