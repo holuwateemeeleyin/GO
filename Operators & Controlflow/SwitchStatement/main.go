24f56d7fed5dcec3e31c0bd979190376eff4bf52
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2025-11-13 22:23:54 +0000

    feat: Implement student score using switch statement

--- Operators & Controlflow/SwitchStatement/main.go
+++ Operators & Controlflow/SwitchStatement/main.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
+// Using a switch statement, determine the score remark of students who attempted a CA test of 10marks
 func main() {
-	var score int = 10
+	var score int
+	fmt.Printf("Enter Student Score: ")
+	fmt.Scanf("%d", &score)
 	switch {
-	case score >= 18:
+	case score >= 16 && score <= 20:
 		fmt.Println("The student passed quite well")
-	case score >= 10:
+	case score >= 10 && score < 16:
 		fmt.Println("The student averagely passed")
 	case score <= 9:
 		fmt.Println("The student failed ")
