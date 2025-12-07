7f70d2b9489741435b13c0a0e98988fac8b205c7
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2025-12-07 22:02:51 +0000

    array

--- Arrays Slices & Map/Arrays/main.go
+++ Arrays Slices & Map/Arrays/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	//array
+	//Array has a fixed length, and elements should be of the same data type
 
 	fruits := [3]string{"apples", "orages", "banana"}
 	fmt.Println(fruits)
