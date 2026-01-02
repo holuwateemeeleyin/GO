3b3536dcac0050f416c47193d22a5f67ddcbf593
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-01-02 23:12:46 +0100

    Slice

--- Arrays Slices & Map/Slices/main.go
+++ Arrays Slices & Map/Slices/main.go
@@ -3,5 +3,8 @@ package main
 func main() {
 	//Slices
 	/* A slice is defined as the contnous segment of an underlying array
-	 */
+	A slice has 3 major components Pointer, length and Capacity
+	Pointers are variables that hold memory addresses
+	The length of a slice is the number of element it contains
+	*/
 }
