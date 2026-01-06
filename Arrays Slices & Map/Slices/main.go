1a9587670ba057e07932bc740b6e74a62cf9c707
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-01-06 20:13:44 +0100

    Learning slices

--- Arrays Slices & Map/Slices/main.go
+++ Arrays Slices & Map/Slices/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Slices
 	/* A slice is defined as the contnous segment of an underlying array
@@ -7,4 +9,21 @@ func main() {
 	Pointers are variables that hold memory addresses
 	The length of a slice is the number of element it contains
 	*/
+	// Declaring a slice
+	slice := []int{0, 48, 58, 47, 84, 4, 8}
+	slices := slice[1:4]
+	fmt.Println(slices)
+	// It will print [48 58 47]
+	fmt.Println("slice capacity:", cap(slice))
+	//This will give 7
+	fmt.Println("slices capacity:", cap(slices))
+	// Slice capacity = number of elements from the slice’s start index to the end of the underlying array
+	// That is, [1:4] will do length of slice(7) minus starting index of slices(1), IT WILL GIVE US 6
+	//declaring and initialising slice with make
+	fmt.Println("Declaring and initialising slice with make")
+	slice = make([]int, 5, 8)
+	fmt.Println("slice:", slice)
+	fmt.Println("slice length:", len(slice))
+	fmt.Println("slice capacity:", cap(slice))
+
 }
