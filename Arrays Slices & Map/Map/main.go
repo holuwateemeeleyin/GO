0e964d9b20d05a587a6ac2d4d6395bac7c31d1ec
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-01-13 23:05:56 +0100

    Map in Go

---
+++ Arrays Slices & Map/Map/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	//Maps are colection of key-value pairs that a user can use to store some data and retrieve it with a key
+	// In Contrast to an array/slice, finding an entry in a map requires a single lookup versus iterating over the entire slice comparing values.
+	//With a large set of Items, this can save time.
+}
