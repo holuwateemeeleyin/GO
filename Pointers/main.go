7e52171be3e5ab4291624ed79279f59505ec1c03
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-02-05 11:31:48 +0100

    Added pointer

--- Pointers/main.go
+++ Pointers/main.go
@@ -1,5 +1,15 @@
 package main
 
-func man() {
+import "fmt"
 
+func main() {
+	// A pointer is a variable that holds memory address of another variable.
+	// They also point where the memory is allocated and provide ways to find or even change the value located at the memory location
+	i := 10
+	fmt.Printf("Pointer datatype and pointer address: %T, %v \n", &i, &i)
+	// Derefrencing to get the dereferenced datatype and actual value
+	fmt.Printf("The dereferenced data type and actual value: %T, %v \n", *(&i), *(&i))
+	// or
+	p := &i
+	fmt.Printf("%T, %v \n", *p, *p)
 }
