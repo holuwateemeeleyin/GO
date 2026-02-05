34a59a3b84e1f4ac8cfffaf5d9c08e0a4e7851e1
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-02-06 00:12:54 +0100

    Added how to initialise pointers

--- Pointers/declaring_initialising_pointer.go
+++ Pointers/declaring_initialising_pointer.go
@@ -10,3 +10,34 @@ func declarePointer() {
 	fmt.Println(i)
 	fmt.Println(s)
 }
+
+// Ways we can initialise a pointer
+// 1. By including the data type
+func initialisePointer1() {
+	x := 10
+	// To initialise a pointer
+	// var <pointer_name> *<data_type> = &<variable_name>
+	var ptr_x *int = &x
+	// We are storing the address of i using the & operator sign
+
+	fmt.Println("The address of x is:", ptr_x)
+}
+
+// 2. Not including the data type: In this case the data type will be determined by the compiler
+// var <pointer_name> = &<variable_name>
+func initialisePointer2() {
+	s := "hello"
+	var ptr_s = &s
+	fmt.Println("The address of s is:", ptr_s)
+	//If you are curious to know the datatype assigned to ptr_s
+	fmt.Printf("The datatype assigned to ptr_s is: %T \n", ptr_s)
+
+}
+
+// 3. Using the shorthand operator
+// we can initialise a pointer this way; <pointer_name> := &<variable_name>
+func initialisePointer3() {
+	s := "Hello world"
+	ptr_s := &s
+	fmt.Println("The address of ptr_s is:", ptr_s)
+}
