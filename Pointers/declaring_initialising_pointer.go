6702212b4f11af956d4d105b74787fdf51345a66
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-02-05 12:08:59 +0100

    declaring a pointer

---
+++ Pointers/declaring_initialising_pointer.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func declarePointer() {
+	//to declare a pointer
+	// var <pointer_name> *<data_type>
+	var i *int
+	var s *string
+	fmt.Println(i)
+	fmt.Println(s)
+}
