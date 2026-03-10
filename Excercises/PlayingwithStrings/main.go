21f4562ada461107afa1629346d615d408f256d3
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-03-10 18:48:15 +0100

    Iterating Over Strings by Rune

--- Excercises/PlayingwithStrings/main.go
+++ Excercises/PlayingwithStrings/main.go
@@ -8,4 +8,9 @@ func Swap(x, y string) (string, string) {
 func main() {
 	a, b := Swap("Hello", "World")
 	fmt.Println("a, b= ", a, b)
+
+	//Iterating Over Strings by Rune
+	for index, runeValue := range "hello" {
+		fmt.Printf("index: %d, value: %c \n", index, runeValue)
+	}
 }
