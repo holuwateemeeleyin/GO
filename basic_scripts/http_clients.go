53322a1b8de6508053aaecc9f798a466d7b15c59
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-03-13 00:40:02 +0100

    feat: Get HTTP Request

--- basic_scripts/http_clients.go
+++ basic_scripts/http_clients.go
@@ -1,5 +1,24 @@
-package basicscripts
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-
+	// HTTP GET Request
+	baseURL := "https://httpbin.org"
+	getResp, err := http.Get(baseURL + "/get")
+	if err != nil {
+		fmt.Println("Error on GET Request:", err)
+		return
+	}
+	defer getResp.Body.Close()
+	getBody, err := io.ReadAll(getResp.Body)
+	if err != nil {
+		fmt.Println("Error reading the body:", err)
+		return
+	}
+	fmt.Println("GET Response:", string(getBody))
 }
