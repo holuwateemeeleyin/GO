301ac98a00d8641e918e517257124d9386e1a249
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-03-16 20:40:11 +0100

    feat: add display task function

--- Projects/Todo/main.go
+++ Projects/Todo/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+// Display todo tasks
+func displayTasks(tasks []string) {
+	for index, task := range tasks {
+		index++
+		fmt.Printf("%d. %s \n", index, task)
+	}
+}
+
 func main() {
+	//Building a Todo app
+	fmt.Println("These are the tasks in your Todo:")
+	tasks := []string{"Apple", "Banana", "Orange"}
+	displayTasks(tasks)
+	fmt.Printf("You have %d items in the list \n", len(tasks))
 
 }
