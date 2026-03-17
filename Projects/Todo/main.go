fd43ca56cdfe852126d722e7dda90b2d2c3b167b
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-03-17 22:52:49 +0100

    fear: add Remove task function

--- Projects/Todo/main.go
+++ Projects/Todo/main.go
@@ -18,10 +18,24 @@ func addTask(taskItems []string, newTask string) {
 	displayTasks(updatedTasks)
 }
 
+// Delete Task
+func removeTask(taskItems []string, delTask string) []string {
+	var updatedTask []string
+	for _, task := range taskItems {
+		if task != delTask {
+			updatedTask = append(updatedTask, task)
+		}
+	}
+	fmt.Println("These are the updated tasks after removing", delTask)
+	displayTasks(updatedTask)
+	return updatedTask
+}
+
 func main() {
 	//Building a Todo app
 	fmt.Println("These are the tasks in your Todo:")
 	tasks := []string{"Apple", "Banana", "Orange"}
 	displayTasks(tasks)
 	addTask(tasks, "Yam")
+	removeTask(tasks, "Apple")
 }
